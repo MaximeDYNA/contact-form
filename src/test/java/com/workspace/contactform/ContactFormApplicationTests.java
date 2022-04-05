0ba371ceca87685cab22471e1294eaaf90434733
MaximeDYNA <maximedyna@gmail.com>
2022-04-05 18:53:20 +0100

    contact-form server with spring

---
+++ src/test/java/com/workspace/contactform/ContactFormApplicationTests.java
@@ -0,0 +1,13 @@
+package com.workspace.contactform;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ContactFormApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
